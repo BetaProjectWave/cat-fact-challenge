e2fe03185d76e7c72d090d551361ac2b6acb38f1
Mike Holmes <mike.holmes@infinityworks.com>
2019-04-10 15:16:28 +0100

    Added @Bean declaration for the RestTemplate

--- src/main/java/uk/co/asto/interview/cats/CatFactApiApplication.java
+++ src/main/java/uk/co/asto/interview/cats/CatFactApiApplication.java
@@ -2,7 +2,10 @@ package uk.co.asto.interview.cats;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Core spring boot application for running the Cat Fact Service
@@ -10,6 +13,11 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan({"uk.co.asto"})
 @SpringBootApplication
 public class CatFactApiApplication {
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.rootUri("https://catfact.ninja").build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CatFactApiApplication.class, args);
