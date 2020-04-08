1de05d967ca2588917fe258395cf46c024e60b82
Mike Holmes <mike.holmes@asto.io>
2020-04-08 13:58:08 +0100

    Amended factscontroller tests to be consistent with breeds controller

--- src/test/java/uk/co/asto/interview/cats/controller/FactControllerTest.java
+++ src/test/java/uk/co/asto/interview/cats/controller/FactControllerTest.java
@@ -5,22 +5,40 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import uk.co.asto.interview.cats.CatFactApiApplication;
+import uk.co.asto.interview.cats.model.FactDTO;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
+@SpringBootTest(
+        classes = { CatFactApiApplication.class },
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class FactControllerTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    private TestRestTemplate restTemplate;
 
     @Test
     public void shouldReturnHttp200OnSuccess() throws Exception {
-        this.mockMvc.perform(get("/facts")).andExpect(status().isOk());
+        ResponseEntity<List<FactDTO>> response = this.restTemplate.exchange(
+                "/facts",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<FactDTO>>() {}
+        );
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 }
